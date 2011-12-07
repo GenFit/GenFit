c0b09bea5280701f866f09d21977192b11612c22
jojo1987 <jojo1987@4ab16672-d4cc-4d65-938a-feac61c82957>
2011-12-07 16:28:06 +0000

    more sophisticated install script

--- trunk/GFRave/install_rave_pandaroot.sh
+++ trunk/GFRave/install_rave_pandaroot.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+# check if necessary environment variables are set
+: ${SIMPATH:?"SIMPATH is not set!"}
+: ${PANDABUILD:?"PANDABUILD is not set!"}
+: ${RAVEPATH:?"RAVEPATH is not set!"}
+
+# set important variables for rave installation
 export CLHEPPATH=$SIMPATH/cern/clhep 
 export CLHEPLIBPATH=$SIMPATH/cern/clhep/lib
 
@@ -7,7 +15,48 @@ export CLHEP_VECTORLIBPATH=$SIMPATH/cern/clhep/lib
 export CLHEP_MATRIXPATH=$SIMPATH/cern/clhep/Matrix
 export CLHEP_MATRIXLIBPATH=$SIMPATH/cern/clhep/lib
 
+
+
 cd $RAVEPATH
-./configure --prefix=$PANDABUILD
-make
-make install
+
+
+buildRave=false
+
+
+if [ ! -f configure ] # bootstrap rave if necessary
+then
+    echo "bootsrap rave"
+    ./bootstrap
+    buildRave=true
+elif [ ! -f installTimeCheck ] # check if file installTimeCheck is there
+then
+    echo "build rave for the first time"
+    buildRave=true
+else # check for new or modified files
+  for file in $RAVEPATH/*
+  do
+    if [ file -nt installTimeCheck ]
+    then
+        echo "found file newer than last build -> rebuild rave"
+        buildRave=true
+        break
+    fi
+  done
+fi
+
+
+if $buildRave
+then
+  # make rave
+  ./configure --prefix=$PANDABUILD
+  make
+  make install
+  
+  touch installTimeCheck # create empty installTimeCheck file
+else
+  echo "rave: nothing to build"
+fi
+
+cd -
+
+
