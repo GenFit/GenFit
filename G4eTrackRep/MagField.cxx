dde8b3c11bacd3ffc78c506a421367ef26ff22c5
philoehler <philoehler@4ab16672-d4cc-4d65-938a-feac61c82957>
2012-03-06 12:57:27 +0000

    Added test DetConstruction and conversion files

---
+++ G4eTrackRep/MagField.cxx
@@ -0,0 +1,64 @@
+/*
+ * MagField.cc
+ *
+ *  Created on: Jan 26, 2012
+ *      Author: poehler
+ */
+
+#include "MagField.hh"
+#include "G4UniformMagField.hh"
+#include "G4FieldManager.hh"
+#include "G4TransportationManager.hh"
+
+
+MagField::MagField()
+: G4UniformMagField(G4ThreeVector()){
+	// TODO Auto-generated constructor stub
+	  GetGlobalFieldManager()->SetDetectorField(this);
+	  GetGlobalFieldManager()->CreateChordFinder(this);
+}
+
+MagField::MagField(G4ThreeVector field)
+: G4UniformMagField(G4ThreeVector(field)){
+	// TODO Auto-generated constructor stub
+	  GetGlobalFieldManager()->SetDetectorField(this);
+	  GetGlobalFieldManager()->CreateChordFinder(this);
+}
+
+
+MagField::~MagField() {
+	// TODO Auto-generated destructor stub
+}
+
+void MagField::SetFieldValue(G4double fieldValue)
+{
+  G4UniformMagField::SetFieldValue(G4ThreeVector(0,0,fieldValue));
+
+}
+
+
+//-------------------------------------------------------------
+void MagField::SetFieldValue(G4ThreeVector fieldVector)
+{
+  // Find the Field Manager for the global field
+  G4FieldManager* fieldMgr= GetGlobalFieldManager();
+
+  if(fieldVector!=G4ThreeVector(0.,0.,0.))
+  {
+    G4UniformMagField::SetFieldValue(fieldVector);
+    fieldMgr->SetDetectorField(this);
+  } else {
+    // If the new field's value is Zero, then it is best to
+    //  insure that it is not used for propagation.
+    G4MagneticField* magField = NULL;
+    fieldMgr->SetDetectorField(magField);
+  }
+}
+
+
+
+//-------------------------------------------------------------
+G4FieldManager*  MagField::GetGlobalFieldManager()
+{
+  return G4TransportationManager::GetTransportationManager()->GetFieldManager();
+}
