923abb69f16c7e4ad0dc877ef9ca086d415ec10c
jojo1987 <jojo1987@4ab16672-d4cc-4d65-938a-feac61c82957>
2012-03-07 12:20:45 +0000

    sets LD_LIBRARY_PATH also to G4LIB and CLHEP_LIB_DIR, needed for G4eTrackRep

--- trunk/makeEnv.sh
+++ trunk/makeEnv.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 ENVFILE=env.sh
 if [ -z $GENFIT ]; then
-echo "define the GENFIT env variable before executing this script"
+  echo "define the GENFIT env variable before executing this script"
 else
-echo "if [ -z \$ROOTSYS ]; then" > $ENVFILE
-echo "echo \"ROOTSYS is not set. Check your ROOT installation.\"" >> $ENVFILE
-echo "else" >> $ENVFILE
-echo "export GENFIT=$GENFIT" >> $ENVFILE
-echo "export VMC=\$GENFIT/geant3" >> $ENVFILE
-echo "if [ \`root-config --arch\` = macosx ]; then" >>$ENVFILE
-echo "export DYLD_LIBRARY_PATH=\$DYLD_LIBRARY_PATH:\$VMC/lib/tgt_macosx:\$GENFIT/lib" >>$ENVFILE
-echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:\$VMC/lib/tgt_macosx:\$GENFIT/lib" >>$ENVFILE
-echo "else" >>$ENVFILE
-echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:\$VMC/lib/tgt_linux:\$GENFIT/lib" >>$ENVFILE
-echo "fi" >>$ENVFILE
-echo "fi" >> $ENVFILE
+  echo "if [ -z \$ROOTSYS ]; then" > $ENVFILE
+    echo "echo \"ROOTSYS is not set. Check your ROOT installation.\"" >> $ENVFILE
+  echo "else" >> $ENVFILE
+    echo "export GENFIT=$GENFIT" >> $ENVFILE
+    echo "export VMC=\$GENFIT/geant3" >> $ENVFILE
+    echo "if [ \`root-config --arch\` = macosx ]; then" >>$ENVFILE
+      echo "export DYLD_LIBRARY_PATH=\$DYLD_LIBRARY_PATH:\$VMC/lib/tgt_macosx:\$GENFIT/lib" >>$ENVFILE
+      echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:\$VMC/lib/tgt_macosx:\$GENFIT/lib" >>$ENVFILE
+    echo "else" >>$ENVFILE
+      echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:\$VMC/lib/tgt_linux:\$GENFIT/lib" >>$ENVFILE
+    echo "fi" >>$ENVFILE
+    echo "if [ -z \$G4LIB ]; then" >> $ENVFILE
+      echo "echo \"G4LIB (needed for G4eTrackRep) is not set. Run env.sh of your Geant4 installation.\"" >> $ENVFILE
+    echo "else" >> $ENVFILE
+      echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:\$G4LIB:\$G4LIB/\$G4SYSTEM" >>$ENVFILE
+    echo "fi" >> $ENVFILE
+    echo "if [ -z \$CLHEP_LIB_DIR ]; then" >> $ENVFILE
+      echo "echo \"CLHEP_LIB_DIR (needed for G4eTrackRep) is not set. Run env.sh of your Geant4 installation.\"" >> $ENVFILE
+    echo "else" >> $ENVFILE
+      echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:\$CLHEP_LIB_DIR" >>$ENVFILE
+    echo "fi" >> $ENVFILE
+  echo "fi" >> $ENVFILE
 fi
