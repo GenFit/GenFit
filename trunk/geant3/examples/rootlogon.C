4049d8ec752460b3b45483b0063ddd117028392b
hoeppi <hoeppi@4ab16672-d4cc-4d65-938a-feac61c82957>
2012-05-17 14:52:47 +0000

    moved geant3 directory out of trunk to top dir of repository

--- trunk/geant3/examples/rootlogon.C
+++
@@ -1,7 +0,0 @@
-{
-   printf("\n*****************************************************\n");
-   printf(  "* Welcome to the GEANT3-VirtualMC examples          *\n");
-   printf(  "*   Type \"gMC->ProcessRun(10)\" to process 10 events *\n");
-   printf(  "*   Type \".q\" to execute the uglast function        *\n");
-   printf(  "*****************************************************\n");
-}
