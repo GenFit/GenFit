d083d3521ecb4f3ef55e42eb60ca3d76b8ca0198
jojo1987 <jojo1987@4ab16672-d4cc-4d65-938a-feac61c82957>
2012-07-04 13:41:29 +0000

    GFRave test macro. Not yet functional

---
+++ trunk/test/GFRaveTest/main.cxx
@@ -0,0 +1,44 @@
+
+#include <iostream>
+#include <GenfitDisplay.h>
+#include <GFConstField.h>
+#include <GFException.h>
+#include <GFFieldManager.h>
+#include <GFTrack.h>
+#include <RKTrackRep.h>
+#include <TApplication.h>
+#include <TCanvas.h>
+#include <TEveManager.h>
+#include <TGeoManager.h>
+#include <TH1D.h>
+#include <TRandom3.h>
+#include <TStyle.h>
+#include <TVector3.h>
+#include <vector>
+
+#include <TROOT.h>
+#include <TFile.h>
+#include <TTree.h>
+#include "TDatabasePDG.h"
+#include <TMath.h>
+#include <TString.h>
+
+
+
+#define VALGRIND
+
+int main() {
+  std::cerr<<"main"<<std::endl;
+
+  const unsigned int nEvents = 100;
+
+
+
+  // main loop
+  for (unsigned int iEvent=0; iEvent<nEvents; ++iEvent){
+
+  }
+      
+
+}
+
---
+++ trunk/test/GFRaveTest/makeGeom.C
@@ -0,0 +1,36 @@
+void makeGeom()
+{
+  //--- Definition of a simple geometry
+  //   gSystem->Load("libGeom");
+   new TGeoManager("genfitGeom", "GENFIT geometry");
+   gROOT->Macro("../../geometry/media.C");
+
+
+   TGeoMedium *vacuum = gGeoManager->GetMedium("vacuum");
+   assert(vacuum!=NULL);
+   TGeoMedium *air = gGeoManager->GetMedium("air");
+   assert(air!=NULL);
+   TGeoMedium *sil = gGeoManager->GetMedium("silicon");
+   assert(sil!=NULL);
+
+   TGeoVolume *top = gGeoManager->MakeBox("TOPPER", air, 1000., 1000., 1000.);
+   gGeoManager->SetTopVolume(top); // mandatory !
+
+   TGeoVolume *redBullCan = gGeoManager->MakeTube("redBullCan", sil, 3.-0.01, 3., 18.);//, 90., 270.);
+   redBullCan->SetLineColor(kRed);
+   top->AddNode(redBullCan, 1, gGeoIdentity);
+
+   TGeoVolume *redBullCan2 = gGeoManager->MakeTube("redBullCan2", sil, 4.-0.01, 4., 20.);//, 90., 270.);
+   redBullCan2->SetLineColor(kRed);
+   top->AddNode(redBullCan2, 1, gGeoIdentity);
+
+   //--- close the geometry
+   gGeoManager->CloseGeometry();
+
+   //--- draw the ROOT box
+   gGeoManager->SetVisLevel(10);
+   //top->Draw("ogl");
+   TFile *outfile = TFile::Open("genfitGeom.root","RECREATE");
+   gGeoManager->Write();
+   outfile->Close();
+}
