31f3db3debaa75931bfb4c091efd84c2c2fa5865
moritznadler <moritznadler@4ab16672-d4cc-4d65-938a-feac61c82957>
2012-09-12 18:12:24 +0000

    first version of the new GFTrackCand. Should be backwards compatable. Several stuff will be removed in the future

--- trunk/core/GFTrackCand.cxx
+++ trunk/core/GFTrackCand.cxx
@@ -15,7 +15,7 @@
 
    You should have received a copy of the GNU Lesser General Public License
    along with GENFIT.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 #include "GFTrackCand.h"
 #include "GFException.h"
 #include"TDatabasePDG.h"
@@ -25,54 +25,70 @@
 
 ClassImp(GFTrackCand)
 
-GFTrackCand::GFTrackCand():fCurv(0),fDip(0),fInv(false), fQoverpSeed(0.),fMcTrackId(-1),fPdg(0){}
+GFTrackCand::GFTrackCand():fCurv(0),fDip(0), fQ(0),fMcTrackId(-1),fPdg(0), fState6D(TMatrixD(TMatrixD::kZero,TMatrixD(6,1))),fCov6D(-1.0*TMatrixD(TMatrixD::kUnit,TMatrixD(6,6))){}
 
 GFTrackCand::~GFTrackCand(){}
 
-GFTrackCand::GFTrackCand(double curv, double dip, double inv, std::vector<unsigned int> detIDs, std::vector<unsigned int> hitIDs)
-  : fDetId(detIDs),fHitId(hitIDs),fCurv(curv), fDip(dip), fInv(inv),fQoverpSeed(0.), fMcTrackId(-1),fPdg(0)
-{
-  assert(fDetId.size()==fHitId.size());
-  fRho.resize(fDetId.size(),0.);
-}
-GFTrackCand::GFTrackCand(double curv, double dip, double inv, std::vector<unsigned int> detIDs, std::vector<unsigned int> hitIDs,std::vector<double> rhos)
-  : fDetId(detIDs),fHitId(hitIDs),fRho(rhos),fCurv(curv), fDip(dip), fInv(inv),fQoverpSeed(0.), fMcTrackId(-1),fPdg(0)
-{
-  assert(fDetId.size()==fHitId.size());
-  assert(fDetId.size()==fRho.size());
-}
+//GFTrackCand::GFTrackCand(double curv, double dip, double inv, std::vector<unsigned int> detIDs, std::vector<unsigned int> hitIDs)
+//  : fDetId(detIDs),fHitId(hitIDs),fCurv(curv), fDip(dip), fInv(inv),fQoverpSeed(0.), fMcTrackId(-1),fPdg(0)
+//{
+//  assert(fDetId.size()==fHitId.size());
+//  fRho.resize(fDetId.size(),0.);
+//}
+//GFTrackCand::GFTrackCand(double curv, double dip, double inv, std::vector<unsigned int> detIDs, std::vector<unsigned int> hitIDs,std::vector<double> rhos)
+//  : fDetId(detIDs),fHitId(hitIDs),fRho(rhos),fCurv(curv), fDip(dip), fInv(inv),fQoverpSeed(0.), fMcTrackId(-1),fPdg(0)
+//{
+//  assert(fDetId.size()==fHitId.size());
+//  assert(fDetId.size()==fRho.size());
+//}
 
 void 
 GFTrackCand::addHit(unsigned int detId, unsigned int hitId, double rho, unsigned int planeId)
 {
-  fDetId.push_back(detId);
-  fHitId.push_back(hitId);
-  fPlaneId.push_back(planeId);
-  fRho.push_back(rho);
+	fDetId.push_back(detId);
+	fHitId.push_back(hitId);
+	fPlaneId.push_back(planeId);
+	fRho.push_back(rho);
 }
 
 std::vector<unsigned int> 
+GFTrackCand::getHitIDs(int detId) const {
+	if(detId<0){ // return hits from all detectors
+		return fHitId;
+	}
+	else {
+		std::vector<unsigned int> result;
+		unsigned int n=fHitId.size();
+		for(unsigned int i=0;i<n;++i){
+			if(fDetId[i]==(unsigned int)detId)result.push_back(fHitId[i]);
+		}
+		return result;
+	}
+}
+
+std::vector<unsigned int>
 GFTrackCand::GetHitIDs(int detId) const {
-  if(detId<0){ // return hits from all detectors
-    return fHitId;
-  }
-  else {
-    std::vector<unsigned int> result;
-    unsigned int n=fHitId.size();
-    for(unsigned int i=0;i<n;++i){
-      if(fDetId[i]==(unsigned int)detId)result.push_back(fHitId[i]);
-    }
-    return result;
-  }
+	std::cerr << "the method GFTrackCand::GetHitIDs is deprecated. Use GFTrackCand::getHitIDs instead\n";
+	if(detId<0){ // return hits from all detectors
+		return fHitId;
+	}
+	else {
+		std::vector<unsigned int> result;
+		unsigned int n=fHitId.size();
+		for(unsigned int i=0;i<n;++i){
+			if(fDetId[i]==(unsigned int)detId)result.push_back(fHitId[i]);
+		}
+		return result;
+	}
 }
 
 void
 GFTrackCand::reset()
 {
-  fDetId.clear();fHitId.clear();
+	fDetId.clear();fHitId.clear();
 }
 
-bool GFTrackCand::HitInTrack(unsigned int detId, unsigned int hitId) const
+bool GFTrackCand::hitInTrack(unsigned int detId, unsigned int hitId) const
 {
 	for (unsigned int i = 0; i < fDetId.size(); i++){
 		if (detId == fDetId[i])
@@ -83,52 +99,58 @@ bool GFTrackCand::HitInTrack(unsigned int detId, unsigned int hitId) const
 }
 
 bool operator== (const GFTrackCand& lhs, const GFTrackCand& rhs){
-  if(lhs.getNHits()!=rhs.getNHits()) return false;
-  bool result=std::equal(lhs.fDetId.begin(),lhs.fDetId.end(),rhs.fDetId.begin());
-  result &=std::equal(lhs.fHitId.begin(),lhs.fHitId.end(),rhs.fHitId.begin());
-  return result;
+	if(lhs.getNHits()!=rhs.getNHits()) return false;
+	bool result=std::equal(lhs.fDetId.begin(),lhs.fDetId.end(),rhs.fDetId.begin());
+	result &=std::equal(lhs.fHitId.begin(),lhs.fHitId.end(),rhs.fHitId.begin());
+	return result;
 }
 
 void GFTrackCand::Print(const Option_t* option) const {
-  std::cout << "======== GFTrackCand::print ========" << std::endl;
-  if(fMcTrackId>=0) std::cout << "mcTrackId=" << fMcTrackId << std::endl;
-  std::cout << "seed values for pos,direction, and q/p: " << std::endl;
-  fPosSeed.Print(option);
-  fDirSeed.Print(option);
-  std::cout << "q/p=" << fQoverpSeed << std::endl;
-  assert(fDetId.size()==fHitId.size());
-  std::cout << "detId|hitId|rho ";
-  for(unsigned int i=0;i<fDetId.size();++i){
-    std::cout << fDetId.at(i) << "|" << fHitId.at(i) 
-	      << "|" << fRho.at(i) << " ";
-  }
-  std::cout << std::endl;
+	std::cout << "======== GFTrackCand::print ========\n";
+	std::cout << "mcTrackId=" << fMcTrackId << "\n";
+	std::cout << "seed values for 6D state and cov: " << std::endl;
+	fState6D.Print(option);
+	fCov6D.Print(option);
+	std::cout << "q" << fQ << "\n";
+	std::cout << "PDG code= " << fPdg << "\n";
+	assert(fDetId.size()==fHitId.size());
+	std::cout << "detId|hitId|rho ";
+	for(unsigned int i=0;i<fDetId.size();++i){
+		std::cout << fDetId.at(i) << "|" << fHitId.at(i)
+	    		  << "|" << fRho.at(i) << " ";
+	}
+	std::cout << std::endl;
 }
 
 void GFTrackCand::append(const GFTrackCand& rhs){
-  unsigned int detId,hitId;
-  double rho;
-  for(unsigned int i=0;i<rhs.getNHits();++i){
-    rhs.getHit(i,detId,hitId,rho);
-    addHit(detId,hitId,rho);
-  }
-
-
+	unsigned int detId,hitId;
+	double rho;
+	for(unsigned int i=0;i<rhs.getNHits();++i){
+		rhs.getHit(i,detId,hitId,rho);
+		addHit(detId,hitId,rho);
+	}
 }
 
-void GFTrackCand::setComplTrackSeed(const TVector3& pos, const TVector3& mom, const int pdgCode, TVector3 posError, TVector3 dirError){
-  fPosSeed=pos; fPdg = pdgCode; fPosError = posError; fDirError = dirError;
-  TVector3 dir = mom;
-  dir.SetMag(1.0);
-  fDirSeed=dir;
-  TParticlePDG* part = TDatabasePDG::Instance()->GetParticle(fPdg);
-  double charge = part->Charge()/(3.);
-  fQoverpSeed=charge/mom.Mag();
+void GFTrackCand::setComplTrackSeed(const TVector3& pos, const TVector3& mom, const int pdgCode, TVector3 posError, TVector3 momError){
+	std::cerr << "the method GFTrackCand::setComplTrackSeed is deprecated. Use GFTrackCand::set6DSeed() or setCurvDipPos() instead\n";
+	setPdgCode(pdgCode); //also sets charge
+	fState6D[0][0] = pos[0];
+	fState6D[1][0] = pos[1];
+	fState6D[2][0] = pos[2];
+	fState6D[3][0] = mom[0];
+	fState6D[4][0] = mom[1];
+	fState6D[5][0] = mom[2];
+	fCov6D[0][0] = posError[0]*posError[0];
+	fCov6D[1][1] = posError[1]*posError[1];
+	fCov6D[2][2] = posError[2]*posError[2];
+	fCov6D[3][3] = momError[0]*momError[0];
+	fCov6D[4][4] = momError[1]*momError[1];
+	fCov6D[5][5] = momError[2]*momError[2];
 }
 
 
 void GFTrackCand::sortHits(){
-	unsigned int nHits = getNHits(); // all 4 private vectors must have the same size.
+	const unsigned int nHits = getNHits(); // all 4 private vectors must have the same size.
 
 	//a vector that will be sorted to give after sort indices to sort the other vectors
 	std::vector<std::pair<double, int> > order(nHits);
@@ -139,7 +161,7 @@ void GFTrackCand::sortHits(){
 
 	std::vector<unsigned int> indices(nHits);
 	for (unsigned int i = 0; i != nHits; ++i){
-	  indices.push_back(order[i].second);
+		indices[i] = order[i].second;
 	}
 
 	sortHits(indices);
@@ -148,11 +170,12 @@ void GFTrackCand::sortHits(){
 
 void GFTrackCand::sortHits(std::vector<unsigned int> indices){
 
-  unsigned int nHits(getNHits());
-  if (indices.size() != nHits){
-    GFException exc("GFTrackCand::sortHits ==> Size of indices != number of hits!",__LINE__,__FILE__);
-    throw exc;
-  }
+	const unsigned int nHits(getNHits());
+	if (indices.size() != nHits){
+		abort();
+		GFException exc("GFTrackCand::sortHits ==> Size of indices != number of hits!",__LINE__,__FILE__);
+		throw exc;
+	}
 
 	//these containers will hold the sorted results. They are created to avoid probably slower in-place sorting
 	std::vector<unsigned int> sortedDetId(nHits);
