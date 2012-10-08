4c87a921fe71f88dc90b3c9fc9219a5f4889c3df
moritznadler <moritznadler@4ab16672-d4cc-4d65-938a-feac61c82957>
2012-10-08 14:39:48 +0000

    new getter and setter using TVector3 (only for user convinience) added some documentation and some other minor stuff

--- trunk/core/GFTrackCand.h
+++ trunk/core/GFTrackCand.h
@@ -38,7 +38,7 @@
  *
  *  @author Christian H&ouml;ppner (Technische Universit&auml;t M&uuml;nchen, original author)
  *  @author Sebastian Neubert  (Technische Universit&auml;t M&uuml;nchen, original author)
- *
+ *  @author Moritz Nadler (maintainer during 2012)
  * The main task of the GFTrackCand object is to store a list of indices to
  * cluster objects. Each cluster in the Track is identified by it's
  * detector ID and it's index in the corresponding TClonesArray.
@@ -50,8 +50,12 @@
  * an arbitrary number of different detectors.
  *
  * In addition GFTrackCand offers members to store starting values for the fit.
- * However this information is not autmatically used in genfit!!!
- * But a pointer to a GFTrackCand can be passed to the RKTrackRep to make use of this information.
+ * The starting values (seeds) for the fit are stored as a 6D state (x,y,z,px,py,pz) and its
+ * corresponding 6x6 covariance matrix. All seed getter and setter manipulate these two members
+ * but the user can chose using TVector3 or TMatrixD to get/set the seed state.
+ * However this information is not automatically used in Genfit.
+ * But a pointer to a GFTrackCand can be passed to the a RKTrackRep constructor
+ * to make use of this information without manually extracting it from the GFTRackCand object.
  *
  * @sa RecoHitFactory
  */
@@ -62,22 +66,6 @@ public:
   // Constructors/Destructors ---------
   GFTrackCand();
   ~GFTrackCand();
-
-  //  /** @brief Initializing constructor
-  //   *
-  //   * @param curv Curvature from prefit. There is no stringent definition what
-  //   * this parameter means at the moment.
-  //   * @param dip Dip angle from prefit. There is no stringent definition what
-  //   * this parameter means at the moment.
-  //   * @param inv Dummy paramter. Has been used to mark inverted tracks
-  //   * in the past.
-  //   * @param detIDs collection of detector IDs. Each detector ID needs
-  //   * a corresponding GFRecoHitProducer. See RecoHitFactory for details.
-  //   * @param hitIDs collection of hit indices.
-  //   */
-  //  GFTrackCand(double curv, double dip, double inv, std::vector<unsigned int> detIDs, std::vector<unsigned int> hitIDs);
-  //  /* @brief same as previous ctor, but with ordering parameters */
-  //  GFTrackCand(double curv, double dip, double inv, std::vector<unsigned int> detIDs, std::vector<unsigned int> hitIDs, std::vector<double> rhos);
 
   /* @brief == operator does not check for rho */
   friend bool operator== (const GFTrackCand& lhs, const GFTrackCand& rhs);
@@ -115,8 +103,12 @@ public:
   }
 
   unsigned int getNHits() const {return fDetId.size();}
-  double getCurv() const {return fCurv;}
-  double getDip() const {return fDip;}
+  double getCurv() const {
+	  std::cerr << "the method GFTrackCand::getCurv() is deprecated. Use the setter/getter for the 6D state or pos,mom instead instead\n";
+	  return fCurv;}
+  double getDip() const {
+	  std::cerr << "the method GFTrackCand::getDip() is deprecated. Use the setter/getter for the 6D state or pos,mom instead instead\n";
+	  return fDip;}
   //bool inverted() const {return fInv;} //nobody seems to use it so I commented it out
   std::vector<unsigned int> getHitIDs(int detId = -1) const;
   std::vector<unsigned int> GetHitIDs(int detId = -1) const;
@@ -137,28 +129,29 @@ public:
   /** @brief get the MCT track id, for MC simulations - def. value -1
    */
   int getMcTrackId() const {return fMcTrackId;}
-  /** @brief get the seed value for track: pos */
+  /** @brief get the seed value for track: pos. Identical to the first 3 components of getStateSeed*/
   TVector3 getPosSeed() const {
-    std::cerr << "the method GFTrackCand::getPosSeed() is deprecated. Use GFTrackCand::getStateSeed() instead\n";
-    TVector3 posSeed(fState6D[0][0], fState6D[1][0], fState6D[2][0]);
+    TVector3 posSeed(fState6D(0,0), fState6D(1,0), fState6D(2,0));
     return posSeed;
+  }
+  /** @brief get the seed value for track: mom. Identical to the last 3 components of getStateSeed*/
+  TVector3 getMomSeed() const {
+    TVector3 momSeed(fState6D(3,0), fState6D(4,0), fState6D(5,0));
+    return momSeed;
   }
   /** @brief get the seed value for track: direction */
   TVector3 getDirSeed() const {
-    std::cerr << "the method GFTrackCand::getDirSeed() is deprecated. Use GFTrackCand::getStateSeed() instead\n";
-    TVector3 dirSeed(fState6D[3][0], fState6D[4][0], fState6D[5][0]);
+    std::cerr << "the method GFTrackCand::getDirSeed() is deprecated. Use GFTrackCand::getStateSeed() or GFTrackCand::getMomSeed() instead\n";
+    double p = std::sqrt(fState6D(3,0) * fState6D(3,0) + fState6D(4,0) * fState6D(4,0) + fState6D(5,0) * fState6D(5,0));
+    TVector3 dirSeed(fState6D(3,0)/p, fState6D(4,0)/p, fState6D(5,0)/p);
     return dirSeed;
   }
   /** @brief get the seed value for track: qoverp */
   double getQoverPseed() const {
-    std::cerr << "the method GFTrackCand::getQoverPseed() is deprecated. Use GFTrackCand::getStateSeed() and/or getChargeSeed() instead\n";
-    double p = std::sqrt(fState6D[3][0] * fState6D[3][0] + fState6D[4][0] * fState6D[4][0] + fState6D[5][0] * fState6D[5][0]);
+    std::cerr << "the method GFTrackCand::getQoverPseed() is deprecated. Use GFTrackCand::getStateSeed() or GFTrackCand::getMomSeed()  and/or getChargeSeed() instead\n";
+    double p = std::sqrt(fState6D(3,0) * fState6D(3,0) + fState6D(4,0) * fState6D(4,0) + fState6D(5,0) * fState6D(5,0));
     return fQ / p;
   }
-  //  /** @brief get the seed value for track: error on pos (standard deviation)*/
-  //  TVector3 getPosError() const {return fPosError;}
-  //  /** @brief get the seed value for track: error on direction (standard deviation)*/
-  //  TVector3 getDirError() const {return fDirError;}
   /** returns the 6D seed state; should be in global coordinates */
   TMatrixD getStateSeed() const {
     return fState6D;
@@ -189,15 +182,11 @@ public:
   /** @brief Test if hit already is part of this track candidate
    */
   bool hitInTrack(unsigned int detId, unsigned int hitId) const;
-  /** @brief set the seed values for track: pos, direction, q/p
-   */
-  //  void setTrackSeed(const TVector3& pos,const TVector3& direction,const double qop){
-  //    fPosSeed=pos;fDirSeed=direction;fQoverpSeed=qop;
-  //  }
+
   /** @brief set the seed values for track: pos, momentum, pdgCode, pos error, momentum error (errors are optional and will be set to 1,1,1 if not given)
    */
   void setComplTrackSeed(const TVector3& pos, const TVector3& mom, const int pdgCode, TVector3 posError = TVector3(1.0, 1.0, 1.0), TVector3 dirError = TVector3(1.0, 1.0, 1.0));
-  /** @brief set a particle hypothesis in form of a PDG code this will also set the charge attribute
+  /** @brief set a particle hypothesis in form of a PDG code. This will also set the charge attribute
    */
   void setPdgCode(int pdgCode) {
     fPdg = pdgCode;
@@ -207,7 +196,7 @@ public:
   void append(const GFTrackCand&);
 
   /** @brief sort the hits that were already added to the trackCand using the rho parameter.
-   * After this function was called rho will determine the order of propagation not the order of the addHit calls
+   * After this function was called rho will determine the order of propagation not the order of the original addHit calls
    */
   void sortHits();
 
@@ -215,6 +204,8 @@ public:
 
   // Operations ----------------------
   void reset();
+  /** @brief write the content of all private attributes to the terminal
+   */
   void Print(const Option_t* = "") const ;
   /** @brief sets the state to seed the track fitting. State has to be a TMatrixD(6,1). First 3 elements are the staring postion second 3 elements the starting momentum. Everything in global coordinates
    * charge is the charge hypotheses of the particle charge
@@ -233,12 +224,33 @@ public:
     fState6D = state6D;
     fCov6D = cov6D;
   }
-//  void setCurvDipPosSeed(const double curvature, const double dipAngle, const TVector3& pos){
-//
-//  };
-//  /** set the errors  */
-//  void setCurvDipPosError(const double curvatureError, const double dipAngleError, const TVector3& posError){};
-
+  /** @brief sets the state to seed the track fitting. State has to be a TMatrixD(6,1). First 3 elements are the staring postion second 3 elements the starting momentum. Everything in global coordinates
+   * charge is the charge hypotheses of the particle charge
+   * ATTENTION: If you set the cov6D covariance matrix of the state remember that there ar VARIANCES not STANDARD DEVIATIONS on the diagonal
+   */
+  void setPosMomSeed(const TVector3& pos, const TVector3& mom, const double charge, TMatrixD cov6D = -1.0 * TMatrixD(TMatrixD::kUnit, TMatrixD(6, 6))) {
+    fQ = charge;
+    fState6D(0,0) = pos[0];
+    fState6D(1,0) = pos[1];
+    fState6D(2,0) = pos[2];
+    fState6D(3,0) = mom[0];
+    fState6D(4,0) = mom[1];
+    fState6D(5,0) = mom[2];
+    fCov6D = cov6D;
+  }
+  /** @brief This function works the same as set6DSeed but instead of a charge hypothesis you can set a pdg code which will set the charge automatically
+   * ATTENTION: If you set the cov6D covariance matrix of the state remember that there are VARIANCES not standard deviations on the diagonal
+   */
+  void setPosMomSeedAndPdgCode(const TVector3& pos, const TVector3& mom, const int pdgCode, TMatrixD cov6D = -1.0 * TMatrixD(TMatrixD::kUnit, TMatrixD(6, 6))) {
+    setPdgCode(pdgCode);
+    fState6D(0,0) = pos[0];
+    fState6D(1,0) = pos[1];
+    fState6D(2,0) = pos[2];
+    fState6D(3,0) = mom[0];
+    fState6D(4,0) = mom[1];
+    fState6D(5,0) = mom[2];
+    fCov6D = cov6D;
+  }
 private:
 
   // Private Data Members ------------
@@ -247,28 +259,21 @@ private:
   std::vector<unsigned int> fPlaneId;
   std::vector<double>       fRho;
 
-  double fCurv; // curvature from pattern reco
-  double fDip;  // dip angle from pattern reco
-  //bool fInv;  // true if inverted track //nobody seems to use it so I commented it out
+  double fCurv; // curvature from pattern reco. Deprecated! Will be deleted soon.
+  double fDip;  // dip angle from pattern reco. Deprecated! Will be deleted soon.
 
-//  TVector3 fPosSeed;  //seed value for the track: pos
-//  TVector3 fDirSeed;  //direction
-//  TVector3 fPosError;  //error on position seed given as a standard deviation
-//  TVector3 fDirError;  //error on direction seed given as a standard deviation
-//  double fQoverpSeed; //q/p
+  int fMcTrackId; /**< if MC simulation, store the mct track id here */
+  int fPdg; /**< particle data groupe's id for a particle*/
 
-  int fMcTrackId; //if MC simulation, store the mct track id here
-  int fPdg; // particle data groupe's id for a particle
-
-  TMatrixD fState6D; /** global 6D position plus momentum state */
-  TMatrixD fCov6D; /** global 6D position plus momentum covariance matrix */
-  double fQ; /** the charge of the particle in units of elementary charge */
+  TMatrixD fState6D; /**< global 6D position plus momentum state */
+  TMatrixD fCov6D; /**< global 6D position plus momentum covariance matrix */
+  double fQ; /**< the charge of the particle in units of elementary charge */
 
 
   // Private Methods -----------------
 
 public:
-  ClassDef(GFTrackCand, 8)
+  ClassDef(GFTrackCand, 9)
 };
 
 #endif
