a1d5e256632b5fa9981663155cdb99b1c550fb50
jojo1987 <jojo1987@4ab16672-d4cc-4d65-938a-feac61c82957>
2012-10-24 16:15:21 +0000

    change includes in same folder to quotation marks instead of chevrons

--- trunk/core/GFDaf.cxx
+++ trunk/core/GFDaf.cxx
@@ -16,7 +16,7 @@
    You should have received a copy of the GNU Lesser General Public License
    along with GENFIT.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include<GFDaf.h>
+#include "GFDaf.h"
 
 GFDaf::GFDaf() : GFKalman::GFKalman() {
 
