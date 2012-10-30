01ee7edd21c30a3f95c07b7ed21d3935ea567b73
philoehler <philoehler@4ab16672-d4cc-4d65-938a-feac61c82957>
2012-10-30 01:11:40 +0000

    GFFastKalman class and PullsTest_fast example added. Eigen library as well.

---
+++ trunk/test/PullsTest_fast/PixHit.h
@@ -0,0 +1,50 @@
+#ifndef PIXHIT_HH
+#define PIXHIT_HH
+
+// Base Class Headers ----------------
+#include "GFRecoHitIfc.h"
+#include "GFPlanarHitPolicy.h"
+#include"GFDetPlane.h"
+
+// Collaborating Class Headers -------
+#include <ostream> // remove if you do not need streaming op
+#include"TRandom3.h"
+
+// Collaborating Class Declarations --
+
+typedef GFRecoHitIfc<GFPlanarHitPolicy> PlanarRecoHit;
+
+class PixHit : public PlanarRecoHit {
+public:
+
+  // Constructors/Destructors ---------
+  PixHit();
+  PixHit(TVector3 point,double x_res, double y_res);
+  PixHit(const GFDetPlane& pl,double res);
+  PixHit(const GFDetPlane& pl,double res, double u, double v);
+
+  virtual ~PixHit();
+
+  virtual GFAbsRecoHit* clone();
+  
+  // Operations ----------------------
+  virtual TMatrixT<double> getHMatrix(const GFAbsTrackRep* stateVector);
+
+
+private:
+  static TRandom3 rand;
+  // Private Data Members ------------
+  static const int NparHitRep = 2;
+
+  // Private Methods -----------------
+
+public:
+  ClassDef(PixHit,1)
+
+};
+
+#endif
+
+//--------------------------------------------------------------
+// $Log$
+//--------------------------------------------------------------
