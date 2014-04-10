54597dadfa7d1cf8fd92a867db4dc37a6684c83f
innenminister <innenminister@4ab16672-d4cc-4d65-938a-feac61c82957>
2014-04-10 13:38:23 +0000

    Another minor simplification.

--- trunk/finitePlanes/include/RectangularFinitePlane.h
+++ trunk/finitePlanes/include/RectangularFinitePlane.h
@@ -44,7 +44,7 @@ class RectangularFinitePlane : public AbsFinitePlane {
   bool isInActive(double u, double v) const;
   void Print(const Option_t* = "") const;
 
-  AbsFinitePlane* clone() const {
+  RectangularFinitePlane* clone() const {
     return new RectangularFinitePlane(*this);
   }
 
