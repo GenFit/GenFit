9ae825ba6031f9482de2a036442f41701bd81852
innenminister <TobiSchluter@users.noreply.github.com>
2014-10-06 11:18:06 +0000

    RKTrackRep: small simplifications to TGeoMaterialInterface, use precalculated logarithms when calculating mean excitation energies.

--- trackReps/src/TGeoMaterialInterface.cc
+++ trackReps/src/TGeoMaterialInterface.cc
@@ -227,17 +227,6 @@ TGeoMaterialInterface::findNextBoundary(const RKTrackRep* rep,
 }
 
 
-double
-TGeoMaterialInterface::findNextBoundaryAndStepStraight(double sMax) {
-
-  gGeoManager->FindNextBoundaryAndStep(sMax);
-  return gGeoManager->GetStep();
-
-}
-
-
-
-
 /*
 Reference for elemental mean excitation energies at:
 http://physics.nist.gov/PhysRefData/XrayMassCoef/tab1.html
@@ -246,33 +235,66 @@ Code ported from GEANT 3
 */
 
 const int MeanExcEnergy_NELEMENTS = 93; // 0 = vacuum, 1 = hydrogen, 92 = uranium
-const double MeanExcEnergy_vals[] = {1.e15, 19.2, 41.8, 40.0, 63.7, 76.0, 78., 82.0, 95.0, 115.0, 137.0, 149.0, 156.0, 166.0, 173.0, 173.0, 180.0, 174.0, 188.0, 190.0, 191.0, 216.0, 233.0, 245.0, 257.0, 272.0, 286.0, 297.0, 311.0, 322.0, 330.0, 334.0, 350.0, 347.0, 348.0, 343.0, 352.0, 363.0, 366.0, 379.0, 393.0, 417.0, 424.0, 428.0, 441.0, 449.0, 470.0, 470.0, 469.0, 488.0, 488.0, 487.0, 485.0, 491.0, 482.0, 488.0, 491.0, 501.0, 523.0, 535.0, 546.0, 560.0, 574.0, 580.0, 591.0, 614.0, 628.0, 650.0, 658.0, 674.0, 684.0, 694.0, 705.0, 718.0, 727.0, 736.0, 746.0, 757.0, 790.0, 790.0, 800.0, 810.0, 823.0, 823.0, 830.0, 825.0, 794.0, 827.0, 826.0, 841.0, 847.0, 878.0, 890.0};
+const double MeanExcEnergy_vals[MeanExcEnergy_NELEMENTS] = {
+  1.e15, 19.2, 41.8, 40.0, 63.7, 76.0, 78., 82.0,
+  95.0, 115.0, 137.0, 149.0, 156.0, 166.0, 173.0, 173.0,
+  180.0, 174.0, 188.0, 190.0, 191.0, 216.0, 233.0, 245.0,
+  257.0, 272.0, 286.0, 297.0, 311.0, 322.0, 330.0, 334.0,
+  350.0, 347.0, 348.0, 343.0, 352.0, 363.0, 366.0, 379.0,
+  393.0, 417.0, 424.0, 428.0, 441.0, 449.0, 470.0, 470.0,
+  469.0, 488.0, 488.0, 487.0, 485.0, 491.0, 482.0, 488.0,
+  491.0, 501.0, 523.0, 535.0, 546.0, 560.0, 574.0, 580.0,
+  591.0, 614.0, 628.0, 650.0, 658.0, 674.0, 684.0, 694.0,
+  705.0, 718.0, 727.0, 736.0, 746.0, 757.0, 790.0, 790.0,
+  800.0, 810.0, 823.0, 823.0, 830.0, 825.0, 794.0, 827.0,
+  826.0, 841.0, 847.0, 878.0, 890.0, };
+// Logarithms of the previous table, used to calculate mixtures.
+const double MeanExcEnergy_logs[MeanExcEnergy_NELEMENTS] = {
+  34.5388, 2.95491, 3.7329, 3.68888, 4.15418, 4.33073, 4.35671, 4.40672, 
+  4.55388, 4.74493, 4.91998, 5.00395, 5.04986, 5.11199, 5.15329, 5.15329, 
+  5.19296, 5.15906, 5.23644, 5.24702, 5.25227, 5.37528, 5.45104, 5.50126, 
+  5.54908, 5.6058, 5.65599, 5.69373, 5.73979, 5.77455, 5.79909, 5.81114, 
+  5.85793, 5.84932, 5.8522, 5.83773, 5.86363, 5.8944, 5.90263, 5.93754, 
+  5.97381, 6.03309, 6.04973, 6.05912, 6.08904, 6.10702, 6.15273, 6.15273, 
+  6.1506, 6.19032, 6.19032, 6.18826, 6.18415, 6.19644, 6.17794, 6.19032, 
+  6.19644, 6.21661, 6.25958, 6.28227, 6.30262, 6.32794, 6.35263, 6.36303, 
+  6.38182, 6.41999, 6.44254, 6.47697, 6.4892, 6.51323, 6.52796, 6.54247, 
+  6.5582, 6.57647, 6.58893, 6.60123, 6.61473, 6.62936, 6.67203, 6.67203, 
+  6.68461, 6.69703, 6.71296, 6.71296, 6.72143, 6.71538, 6.67708, 6.7178, 
+  6.71659, 6.73459, 6.7417, 6.77765, 6.79122, };
 
 
 double
-MeanExcEnergy_get(int Z) {
+MeanExcEnergy_get(int Z, bool logs = false) {
   assert(Z >= 0 && Z < MeanExcEnergy_NELEMENTS);
-  return MeanExcEnergy_vals[Z];
+  if (logs)
+    return MeanExcEnergy_vals[Z];
+  else
+    return MeanExcEnergy_logs[Z];
 }
 
 
 double
 MeanExcEnergy_get(TGeoMaterial* mat) {
   if (mat->IsMixture()) {
+    // The mean excitation energy is calculated as the geometric mean
+    // of the mEEs of the components, weighted for abundance.
     double logMEE = 0.;
     double denom  = 0.;
     TGeoMixture* mix = (TGeoMixture*)mat;
     for (int i = 0; i < mix->GetNelements(); ++i) {
-      int index = int(floor((mix->GetZmixt())[i]));
-      logMEE += 1. / (mix->GetAmixt())[i] * (mix->GetWmixt())[i] * (mix->GetZmixt())[i] * log(MeanExcEnergy_get(index));
-      denom  += (mix->GetWmixt())[i] * (mix->GetZmixt())[i] * 1. / (mix->GetAmixt())[i];
+      int index = int(mix->GetZmixt()[i]);
+      double weight = mix->GetWmixt()[i] * mix->GetZmixt()[i] / mix->GetAmixt()[i];
+      logMEE += weight * MeanExcEnergy_get(index, true);
+      denom  += weight;
     }
     logMEE /= denom;
     return exp(logMEE);
-  } else { // not a mixture
-    int index = int(floor(mat->GetZ()));
-    return MeanExcEnergy_get(index);
-  }
+  } 
+
+  // not a mixture
+  int index = int(mat->GetZ());
+  return MeanExcEnergy_get(index, false);
 }
 
 
