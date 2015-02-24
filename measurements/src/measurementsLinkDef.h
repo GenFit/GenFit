12a84725a02c4bc67a7c6193ed2c85832ad86166
innenminister <TobiSchluter@users.noreply.github.com>
2015-02-24 13:03:10 +0000

    Add a variant of the WireMeasurement class that occupies significantly less memory.

--- measurements/src/measurementsLinkDef.h
+++ measurements/src/measurementsLinkDef.h
@@ -17,6 +17,7 @@
 #pragma link C++ class genfit::HMatrixUV+;
 #pragma link C++ class genfit::ProlateSpacepointMeasurement+;
 #pragma link C++ class genfit::WireMeasurement+;
+#pragma link C++ class genfit::WireMeasurementNew+;
 #pragma link C++ class genfit::WirePointMeasurement+;
 
 #pragma link C++ class genfit::HMatrixPhi-;
