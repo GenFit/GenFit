047dff0183d02f33722af26c2b67843cb7082941
innenminister <TobiSchluter@users.noreply.github.com>
2015-08-12 13:40:41 +0000

    Prepare for merge to Belle2.

--- fitters/src/DAF.cc
+++ fitters/src/DAF.cc
@@ -129,7 +129,7 @@ void DAF::processTrackWithRep(Track* tr, const AbsTrackRep* rep, bool resortHits
     try{
       converged = calcWeights(tr, rep, betas_.at(iBeta));
       if (!converged && iBeta >= minIterations_-1 &&
-          status->getBackwardPVal() != 0 && fabs(lastPval - status->getBackwardPVal()) < this->deltaPval_) {
+          status->getBackwardPVal() != 0 && abs(lastPval - status->getBackwardPVal()) < this->deltaPval_) {
         if (debugLvl_ > 0) {
           std::cout << "converged by Pval = " << status->getBackwardPVal() << " even though weights changed at iBeta = " << iBeta << std::endl;
         }
