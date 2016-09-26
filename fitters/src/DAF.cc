459c2dd5f01b0f6244723e462ede7eec8ca6050b
Thomas Hauth <Thomas.Hauth@kit.edu>
2016-09-26 13:29:19 +0200

    Changed to use floating-point version in early termination criteria in DAF. Before, all values < 1.0 were truncated to 0.0

--- fitters/src/DAF.cc
+++ fitters/src/DAF.cc
@@ -131,7 +131,7 @@ void DAF::processTrackWithRep(Track* tr, const AbsTrackRep* rep, bool resortHits
     try{
       converged = calcWeights(tr, rep, betas_.at(iBeta));
       if (!converged && iBeta >= minIterations_-1 &&
-          status->getBackwardPVal() != 0 && abs(lastPval - status->getBackwardPVal()) < this->deltaPval_) {
+          status->getBackwardPVal() != 0 && fabs(lastPval - status->getBackwardPVal()) < this->deltaPval_) {
         if (debugLvl_ > 0) {
           debugOut << "converged by Pval = " << status->getBackwardPVal() << " even though weights changed at iBeta = " << iBeta << std::endl;
         }
