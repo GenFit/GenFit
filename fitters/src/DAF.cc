5fbe5b46c21772f6907b635dd85f074043bc513f
Markus Prim <markus.prim@kit.edu>
2016-11-25 14:52:56 +0100

    Remove uncalled code in DAF.

--- fitters/src/DAF.cc
+++ fitters/src/DAF.cc
@@ -191,43 +191,6 @@ void DAF::addProbCut(const double prob_cut, const int measDim){
   chi2Cuts_[measDim] = ROOT::Math::chisquared_quantile_c( prob_cut, measDim);
 }
 
-
-void DAF::setBetas(double b1,double b2,double b3,double b4,double b5,double b6,double b7,double b8,double b9,double b10){
-  betas_.clear();
-  assert(b1>0);betas_.push_back(b1);
-  if(b2>0){
-    assert(b2<=b1);betas_.push_back(b2);
-    if(b3>=0.) {
-      assert(b3<=b2);betas_.push_back(b3);
-      if(b4>=0.) {
-        assert(b4<=b3);betas_.push_back(b4);
-        if(b5>=0.) {
-          assert(b5<=b4);betas_.push_back(b5);
-          if(b6>=0.) {
-            assert(b6<=b5);betas_.push_back(b6);
-            if(b7>=0.) {
-              assert(b7<=b6);betas_.push_back(b7);
-              if(b8>=0.) {
-                assert(b8<=b7);betas_.push_back(b8);
-                if(b9>=0.) {
-                  assert(b9<=b8);betas_.push_back(b9);
-                  if(b10>=0.) {
-                    assert(b10<=b9);betas_.push_back(b10);
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-  minIterations_ = betas_.size();
-  maxIterations_ = betas_.size() + 4;
-  betas_.resize(maxIterations_,betas_.back()); //make sure main loop has a maximum of maxIterations_ and also make sure the last beta value is used for if more iterations are needed then the ones set by the user.
-}
-
-
 void DAF::setAnnealingScheme(double bStart, double bFinal, unsigned int nSteps) {
   // The betas are calculated as a geometric series that takes nSteps
   // steps to go from bStart to bFinal.
