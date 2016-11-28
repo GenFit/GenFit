d991f2ccf660cc83d17e35c8309200c2ac08efa1
Markus Prim <markus.prim@kit.edu>
2016-11-28 17:11:30 +0100

    Add header for all Eigen Matrix typedefs.

---
+++ core/include/EigenMatrixTypedefs.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <eigen3/Eigen/Dense>
+
+namespace genfit {
+    typedef double Precision;
+
+    typedef Precision Scalar;
+
+    typedef Eigen::Matrix<Precision, 3, 1> Vector3;
+    typedef Eigen::Matrix<Precision, 5, 1> Vector5;
+    typedef Eigen::Matrix<Precision, 6, 1> Vector6;
+    typedef Eigen::Matrix<Precision, 7, 1> Vector7;
+
+    typedef Eigen::Matrix<Precision, 3, 3> Matrix3x3;
+    typedef Eigen::Matrix<Precision, 5, 5> Matrix5x5;
+    typedef Eigen::Matrix<Precision, 6, 6> Matrix6x6;
+    typedef Eigen::Matrix<Precision, 7, 7> Matrix7x7;
+
+    typedef Eigen::Matrix<Precision, Eigen::Dynamic, 1> VectorDynamic;
+    typedef Eigen::Matrix<Precision, Eigen::Dynamic, Eigen::Dynamic> MatrixDynamic;
+}
