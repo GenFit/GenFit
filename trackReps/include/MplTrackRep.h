65f39b122b938cd7d18d402c275a0ca73ad303ce
Dmitrii Neverov <dmitrii.neverov@desy.de>
2019-07-02 12:08:40 +0900

    updated copyright headers

--- trackReps/include/MplTrackRep.h
+++ trackReps/include/MplTrackRep.h
@@ -1,12 +1,21 @@
-/**************************************************************************
- * BASF2 (Belle Analysis Framework 2)                                     *
- * Copyright(C) 2019 - Belle II Collaboration                             *
- *                                                                        *
- * Author: The Belle II Collaboration                                     *
- * Contributors: Dmitrii Neverov                                          *
- *                                                                        *
- * This software is provided "as is" without any warranty.                *
- **************************************************************************/
+/* Copyright 2019, Belle II Collaboration
+   Authors: Dmitrii Neverov
+
+   This file is part of GENFIT.
+
+   GENFIT is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published
+   by the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   GENFIT is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with GENFIT.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "RKTrackRep.h"
 
