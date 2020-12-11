97835189fba70beb994d963a18ed75521f6f1c8f
Markus Prim <markus.prim@desy.de>
2020-12-11 10:58:12 +0100

    Continue purging. Remove tests in CMakeLists gtests section of removed files. Remove typdefs header. Undo the change where we actually used the typdef and go back to simply using double.

--- core/include/Material.h
+++ core/include/Material.h
@@ -1,22 +1,20 @@
 #ifndef genfit_Material_h
 #define genfit_Material_h
 
-#include "EigenMatrixTypedefs.h"
-
 #include <TObject.h>
 
 namespace genfit {
 
     struct Material {
-        Scalar density;  /// Density in g / cm^3
-        Scalar Z;  /// Atomic number
-        Scalar A;  /// Mass number in g / mol
-        Scalar radiationLength;  /// Radiation Length in cm
-        Scalar mEE;  /// Mean excitaiton energy in eV
+        double density;  /// Density in g / cm^3
+        double Z;  /// Atomic number
+        double A;  /// Mass number in g / mol
+        double radiationLength;  /// Radiation Length in cm
+        double mEE;  /// Mean excitaiton energy in eV
 
         Material() : density(0), Z(0), A(0), radiationLength(0), mEE(0) {}
 
-        Material(Scalar density_, Scalar Z_, Scalar A_, Scalar radiationLength_, Scalar mEE_) :
+        Material(double density_, double Z_, double A_, double radiationLength_, double mEE_) :
                 density(density_), Z(Z_), A(A_), radiationLength(radiationLength_), mEE(mEE_) {}
 
         Material(const Material &material) = default;
