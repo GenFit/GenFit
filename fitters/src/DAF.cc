cce81a508bcf379d0b5b544b7401f7d2fdad75d2
Giacomo De Pietro <giacomo.pietro@kit.edu>
2023-05-31 17:27:29 +0200

    Fix compiler warning about private members initialized in the wrong order

--- fitters/src/DAF.cc
+++ fitters/src/DAF.cc
@@ -41,7 +41,7 @@
 namespace genfit {
 
 DAF::DAF(std::tuple<double, double, int> annealingScheme, int minIter, int maxIter, int minIterForPval, bool useRefKalman, double deltaPval, double deltaWeight, double probCut)
-  : AbsKalmanFitter(10, deltaPval), deltaWeight_(deltaWeight), minIterForPval_(minIterForPval)
+  : AbsKalmanFitter(10, deltaPval), minIterForPval_(minIterForPval), deltaWeight_(deltaWeight)
 {
   if (useRefKalman) {
     kalman_.reset(new KalmanFitterRefTrack());
