fc423f9a2b932ead6c024e425df2cbb230991d56
Christian Wessel <christian.wessel@desy.de>
2026-08-26 09:42:10 +0200

    Forgot one line.

--- GBL/src/GblFitter.cc
+++ GBL/src/GblFitter.cc
@@ -158,7 +158,7 @@ void GblFitter::processTrackWithRep(Track* trk, const AbsTrackRep* rep, bool res
   // ------------------------------------------------------------------- 
   for (unsigned int iIter = 0; iIter < m_externalIterations; iIter++) {
     // GBL refit (1st of reference, then refit of GBL trajectory itself)
-    int nscat = 0, nmeas = 0, ndummy = 0;
+    [[maybe_unused]] int nscat = 0, nmeas = 0, ndummy = 0; // variables are all used, but compiler doesn't understand
     std::vector<gbl::GblPoint> points = collectGblPoints(trk, rep);
     for(unsigned int ip = 0;ip<points.size(); ip++) {
       GblPoint & p = points.at(ip);
